4d7938ad37d67e8a2cb65bbe46633998e939f5e5
vanditabyaa09 <vanditabyaa0908@gmail.com>
2026-03-02 12:48:04 +0530

    Initial commit: Insurance premium prediction API

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, computed_field, Field, field_validator
+from typing import Literal, Annotated
+import pickle 
+import pandas as pd
+from schema.user_input import UserInput
+from model.predict import predict_output, model, MODEL_VERSION
+
+app = FastAPI()
+        
+# human readable
+@app.get("/")
+def home(): 
+    return {'message':'insurance premium prediction API'}
+
+# machine readble -> some cloud services force us to do this
+@app.get("/health")
+def health_check():
+    return{
+        'status' : 'OK',
+        'version' : MODEL_VERSION,
+        'model_loaded' : model is not None
+    }
+
+@app.post('/predict') 
+def predict_premium(data: UserInput):
+
+    user_input = {
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }
+
+    try:
+        prediction = predict_output(user_input)
+        return JSONResponse(status_code=200, content={'predicted_category': prediction})
+    
+    except Exception as e:
+        return JSONResponse(status_code = 500, content = str(e))
+    
---
+++ model/predict.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import pickle 
+# import the ml model
+with open('model/model.pkl', 'rb') as f :
+    model = pickle.load(f)
+
+# Generally MLFlow will give us the 
+MODEL_VERSION = '1.0.0'
+
+# get class labels from model (important for matching probabilities to class names)
+class_labels = model.classes_.tolist()
+
+def predict_output(user_input : dict) :
+    df = pd.DataFrame([user_input])
+
+    # predict the class 
+    predicted_class = model.predict(df)[0]
