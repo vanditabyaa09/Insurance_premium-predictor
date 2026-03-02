e4fafd30f92d0a106b025ab015516866b49b075f
vanditabyaa09 <vanditabyaa0908@gmail.com>
2026-03-02 13:19:05 +0530

    Model predict

--- model/predict.py
+++ model/predict.py
@@ -15,3 +15,14 @@ def predict_output(user_input : dict) :
 
     # predict the class 
     predicted_class = model.predict(df)[0]
+
+    ## Get probabilities for all classes 
+    probabilities = model.predict_proba(df)[0]
+    confidence = max(probabilities)
+
+    # Create mapping: {class_name: probability}
+    class_probs = dict(zip(class_labels, map(lambda p : round(p, 4), probabilities)))
+
+    return {
+        
+    }
